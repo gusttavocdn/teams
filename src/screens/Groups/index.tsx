9e97864ba509db92eefd2c64474d23c5777fb78b
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-02-08 12:06:59 -0300

    feat: add highlight component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,13 @@
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
 import { Container } from './styles';
 
 export function Groups() {
   return (
     <Container>
       <Header />
+
+      <Highlight title='Grupos' subtitle='Crie e gerencie seus grupos' />
     </Container>
   );
 }
