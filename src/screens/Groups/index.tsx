2dbec9c24870cc4fcf3f1556602fa546f132df9e
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-02-08 12:32:55 -0300

    feat: add ListEmpty Component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,16 +1,30 @@
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { ListEmpty } from '@components/ListEmpty';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 import { Container } from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <Container>
       <Header />
 
       <Highlight title='Grupos' subtitle='Crie e gerencie seus grupos' />
 
-      <GroupCard title='Galera do Ignite' />
+      <FlatList
+        style={{ width: '100%' }}
+        data={groups}
+        keyExtractor={(item, i) => `${item}-${i}`}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message='Que tal cadastrar a primeira turma' />
+        )}
+      />
     </Container>
   );
 }
