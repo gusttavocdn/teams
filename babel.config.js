6333c63b1f84110e577a4843fab4ad7bfb557e02
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-02-07 21:07:22 -0300

    chore: initial configs

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@routes': './src/routes',
+          '@screens': './src/screens',
+          '@store': './src/storage',
+          '@theme': './src/theme',
+          '@utils': './src/utils',
+        },
+      },
+    ],
   };
 };
---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,5 @@
+import { View } from 'react-native';
+
+export function Groups() {
+  return <View></View>;
+}
