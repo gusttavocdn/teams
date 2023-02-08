8a8899bfc8171e819fb885b521e9df36c6681217
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-02-08 10:20:22 -0300

    feat: add fonts and loading component

--- App.tsx
+++ App.tsx
@@ -1,9 +1,23 @@
 import { Groups } from '@screens/Groups';
 import { ThemeProvider } from 'styled-components';
-
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+import { Loading } from '@components/Loading';
 import theme from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  if (fontsLoaded)
+    return (
+      <ThemeProvider theme={theme}>
+        <Loading />
+      </ThemeProvider>
+    );
+
   return (
     <ThemeProvider theme={theme}>
       <Groups />
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,13 +2,12 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  background-color: #FFF;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
   align-items: center;
   justify-content: center;
 `;
 
-
 export const Title = styled.Text`
-  color: #FFF;
+  color: #fff;
   font-size: 32px;
-`
+`;
